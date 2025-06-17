de930d77872043f8cfd91e88bb7e35547e84c7b7
Sushanth Ravishankar <sushanthpvr@gmail.com>
2025-06-16 23:32:11 -0400

    third commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "realestate-1b5cc.firebaseapp.com",
+  projectId: "realestate-1b5cc",
+  storageBucket: "realestate-1b5cc.firebasestorage.app",
+  messagingSenderId: "886351389850",
+  appId: "1:886351389850:web:2a198b7a025267b36beb23"
 };
 
 // Initialize Firebase
